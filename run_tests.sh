626b5a5189121381817906c5ef510f5e63d92c54
Alejandra García Rojas Martínez <alegrm@gmail.com>
2019-08-23 12:29:37 +0200

    fix automate

--- run_tests.sh
+++ run_tests.sh
@@ -4,10 +4,10 @@ pip install --upgrade pip  # not sure this is needed
 pip install pyshacl
 pip install pytest
 # fix owlrl issue  https://github.com/RDFLib/OWL-RL/issues/29#issuecomment-484773978
-mv neuroshapes-tests/bin/owlrl.py neuroshapes-tests/bin/owlrl
+mv env/bin/owlrl.py env/bin/owlrl
 # run tests
 pytest tests/validating_schemas_pytest.py
 
 # clean TestSuite
 deactivate
-rm -r neuroshapes-tests
+rm -r env # not sure this is needed
