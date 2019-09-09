f4a644761696d166f5b24973490ae0772f0dab04
Alejandra García Rojas Martínez <alegrm@gmail.com>
2019-09-09 14:19:34 +0200

    bit of clean up

--- tests/validating_schemas.py
+++ tests/validating_schemas.py
@@ -1,4 +1,4 @@
-import sys,os
+import sys, os
 import pytest
 import logging
 from validator_helper import Validator
@@ -12,21 +12,19 @@ def test_valid_schema_file(shacl_validator, schema_file):
 
 
 def test_valid_data(data_shape_file, test_file, test_valid):
-
     conforms, results, report = make_validation(data_shape_file, test_file)
     if test_valid:
         assert conforms, report
     else:
-        assert conforms==False, report
+        assert conforms is False, report
         assert results == 1, "Validation report expects only 1 entry"
 
 
 def make_validation(shapes_file, test_file):
-
     data_validator = Validator(pytest.SHACL_SHACL)
     data_validator.load_schema(shapes_file)
 
-    datashape_example = shapes_file.replace('schema.json', os.path.join('examples','datashapes.json'))
+    datashape_example = shapes_file.replace('schema.json', os.path.join('examples', 'datashapes.json'))
     if os.path.exists(datashape_example):
         data_validator.load_schema(datashape_example, aggregate=True)
 
--- tests/validator_helper.py
+++ tests/validator_helper.py
@@ -9,13 +9,21 @@ logger.level = logging.DEBUG
 SH = rdflib.Namespace('http://www.w3.org/ns/shacl#')
 RDF = rdflib.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 
+
 def get_graph(file):
-    filetype = rdflib.util.guess_format(file)
-    if filetype == None:
-        filetype="json-ld"
+    file_type = rdflib.util.guess_format(file)
+    if file_type is None:
+        file_type = "json-ld"
     graph = rdflib.Graph()
-    graph.parse(file, format=filetype)
+    graph.parse(file, format=file_type)
     return graph
+
+
+def run_validation(data, schema):
+    conforms, v_graph, v_text = validate(data, shacl_graph=schema, inference='rdfs', debug=False)
+    results = [r for r in v_graph.triples((None, RDF.type, SH.ValidationResult))]
+    return conforms, len(results), v_text
+
 
 class Validator:
 
@@ -25,9 +33,9 @@ class Validator:
 
     def load_schema(self, schema_file, aggregate=False):
         schema_tmp = get_graph(schema_file)
-        if not aggregate :
+        if not aggregate:
             self.schema_graph = rdflib.Graph()
-        conforms, res, report = self.run_validation(schema_tmp, self.shacl_schema)
+        conforms, res, report = run_validation(schema_tmp, self.shacl_schema)
         if conforms:
             self.schema_graph = self.schema_graph + schema_tmp
             logger.debug(f'{schema_file} loaded with {len(schema_tmp)} triples')
@@ -39,9 +47,4 @@ class Validator:
 
     def validate_data_file(self, data_file):
         data_graph = get_graph(data_file)
-        return self.run_validation(data_graph, self.schema_graph)
-
-    def run_validation(self, data, schema):
-        conforms, v_graph, v_text = validate(data, shacl_graph=schema, inference='rdfs', debug=False)
-        results = [r for r in v_graph.triples( (None, RDF.type, SH.ValidationResult) )]
-        return (conforms, len(results), v_text)
+        return run_validation(data_graph, self.schema_graph)
