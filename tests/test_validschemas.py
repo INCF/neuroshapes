dd1e147e3c52cd82f47748a75723d012676eae4a
Alejandra Garcia Rojas M <alegrm@gmail.com>
2019-11-22 12:30:42 +0100

    Refactoring tests
    * moved data examples to a separated directory
    * fixed test to this new logic

--- tests/test_validschemas.py
+++ tests/test_validschemas.py
@@ -1,25 +1,46 @@
-from .shaclvalidator import Validator
-from .shaclvalidator import run_validation
-from .shaclvalidator import get_graph
-import pytest
+"""Testing functions"""
 import logging
 
-logger = logging.getLogger()
-logger.level = logging.DEBUG
+import pytest
+from rdflib import Graph
+
+from tests.shaclvalidator import add_file_to_graph, run_validation
+
+LOGGER = logging.getLogger(__name__)
 
 
-def test_valid_schema_file(shacl_schema, schema_file):
-    logger.debug(f'validating {schema_file}')
-    graph = get_graph(schema_file)
-    conforms, res, report = run_validation(graph, shacl_schema)
+def test_valid_schema_file(shacl_schema: Graph, schema_file: str):
+    """Validates a SHACL shape conforms to a SHACL schema
+
+    Args:
+        :param shacl_schema: the graph whith the SHACL schema loaded
+        :param schema_file: the path of the schema to validate
+    """
+    LOGGER.debug("validating %s", schema_file)
+    graph = Graph()
+    add_file_to_graph(graph, schema_file, pytest.shape_to_file)
+    conforms, _, report = run_validation(graph, shacl_schema)
     assert conforms, "shape does not conform with SHACL of SHACL: " + report
+    if conforms is False:
+        LOGGER.info(report)
 
 
-def test_examples(shapes_file: str, test_file: str, test_valid: bool):
-    validator = Validator(pytest.SHACL_SHACL)
-    validator.load_schema(shapes_file)
-    conforms, results, report = validator.validate_data_file(test_file)
-    if test_valid:
+def test_examples(shapes_file, test_file, assertion):
+    """Test validity or invalidity of data against a SHACL shape
+
+    Args:
+        :param shapes_file: file where the SHACL shape is
+        :param test_file: the data file to validate
+        :param assertion: specify what to test validity or invalidity
+    """
+    LOGGER.debug("testing %s", test_file)
+    shapes_graph = Graph()
+    add_file_to_graph(shapes_graph, shapes_file, pytest.shape_to_file)
+    data_graph = Graph()
+    add_file_to_graph(data_graph, test_file)
+    conforms, results, report = run_validation(data_graph, shapes_graph)
+
+    if assertion:
         assert conforms, report
     else:
         assert conforms is False, "validation should fail"
